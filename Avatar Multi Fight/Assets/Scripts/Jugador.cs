ed7abdb8580fd53f1fc01f775034144847131435
basilio007 <113118989+basilio007@users.noreply.github.com>
2023-02-08 18:06:26 +0100

    hy

--- Avatar Multi Fight/Assets/Scripts/Jugador.cs
+++ Avatar Multi Fight/Assets/Scripts/Jugador.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 
 public class Jugador : MonoBehaviour
 {
-    private Animator animator;
-    public float Speed;
+   
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        animator.SetFloat("Velocidad", Speed);
+      
     }
 }
