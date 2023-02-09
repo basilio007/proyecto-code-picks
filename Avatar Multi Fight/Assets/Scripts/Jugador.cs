2648e5c71ad4c4000f8cc9a80731fb754f49d5eb
basilio007 <113118989+basilio007@users.noreply.github.com>
2023-02-09 17:11:05 +0100

    man with his all animation
    the animation has already  beencreated but has not been activaten to be uses by keys

--- Avatar Multi Fight/Assets/Scripts/Jugador.cs
+++ Avatar Multi Fight/Assets/Scripts/Jugador.cs
@@ -4,16 +4,38 @@ using UnityEngine;
 
 public class Jugador : MonoBehaviour
 {
-   
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed; //variable velocidad de traslado de un punto a otro
+    public Rigidbody2D salto;//variable rigidbody del salto
+    public float fuerza; //variable control de fuerza en el  salto
+
+    //variables de control del personajecon el suelo
+    public Transform checker;
+    public float radiofloor;
+    public bool touchfloor;
+    public LayerMask isfloor;
 
     // Update is called once per frame
     void Update()
     {
-      
+        EjecutarMovement();
+        if (Input.GetKey(KeyCode.Space))
+        {
+           
+        }
+       
+
+
+    }
+
+
+    void EjecutarMovement()
+    {
+        float inputMovement = Input.GetAxis("Horizontal");
+        Rigidbody2D Rigidbody = GetComponent<Rigidbody2D>();
+
+        Rigidbody.velocity = new Vector2(inputMovement * speed, Rigidbody.velocity.y);
+
+
+
     }
 }
