86d2f813a870c38e31a29bd2a44e0f1056f0cbaa
basilio007 <113118989+basilio007@users.noreply.github.com>
2023-03-15 16:24:29 +0100

    aaaa

--- Avatar Multi Fight/Assets/Scripts/barradevida.cs
+++ Avatar Multi Fight/Assets/Scripts/barradevida.cs
@@ -14,6 +14,10 @@ public class barradevida : MonoBehaviour
 
     public float vidamaxima;
 
+    public GameObject signObject;
+    public GameObject enemy;
+
+    private bool activated = false;
 
     // Update is called once per frame
     void Update()
@@ -21,13 +25,51 @@ public class barradevida : MonoBehaviour
         
         vida.fillAmount = vidaactual / vidamaxima;
 
-    }
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if (collision.collider.CompareTag("Enemigo"))
-        {
 
-            vidaactual = 85;
-        }
     }
+  
+
+    /*private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("EL trigger ha entrado");
+
+        if(other.tag == "Enemigo")
+        {
+            signObject.setActive(true);
+            if (!activated)
+            {
+                activated = true;
+                enemy.SetActive(true);
+            }
+        }
+
+
+    }*/
+
+   /* private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("ha salido");
+
+        if (other.tag == "Enemigo")
+        {
+            signObject.setActive(false);
+           
+            
+        }
+
+
+    }
+    */
+
+
+
+
+    private void OnCollisionStay2D(Collision2D collision)
+     {
+         if (collision.collider.CompareTag("Enemigo"))
+         {
+
+             vidaactual = 85;
+         }
+     }
 }
