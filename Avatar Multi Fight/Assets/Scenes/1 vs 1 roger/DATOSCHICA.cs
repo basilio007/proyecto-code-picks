96eb2b31932b089b9e4d5c25149d8bbde22194bc
Ichart-s <aichart28@gmail.com>
2023-04-12 17:07:06 +0200

    SCRIPTS PER PASSAR PODER CAMBIAR I PODER FER 1VS 1

---
+++ Avatar Multi Fight/Assets/Scenes/1 vs 1 roger/DATOSCHICA.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DATOSCHICA : MonoBehaviour
+{
+   /* public static int vidaPlayer;
+    public int vida_Vieja;
+    public Slider1 vidaVisual;
+    Animator Anim2;
+    public GameObject END;
+
+
+    public event EventHandler MuerteJugadora;
+
+    void Start()
+    {
+        Anim2 = gameObject.GetComponent<Animator>();
+        vidaPlayer = 100;
+        vida_Vieja = 100;
+    }
+
+
+    private void Update()
+    {
+        vidaVisual.GetComponent<Slider1>().value = vidaPlayer;
+
+        if (vidaPlayer != vida_Vieja)
+        {
+            Anim2.SetTrigger("daño");
+            Debug.Log(vida_Vieja);
+
+            vida_Vieja = vidaPlayer;
+        }
+
+
+        if (vidaPlayer <= 0)
+        {
+            MuerteJugadora?.Invoke(this, EventArgs.Empty);
+            END.SetActive(true);
+
+            Debug.Log("GAME OVER");
+        }
+
+    }
+   */
+
+}
