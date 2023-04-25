ba5c123d0fc21e609c1a89d0053eace8b166fd7a
Ichart-s <aichart28@gmail.com>
2023-04-25 18:07:30 +0200

    menu pause

---
+++ Avatar Multi Fight/Assets/MenuPause.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPause : MonoBehaviour
+
+{
+    [SerializeField] private GameObject botonPause;
+
+    [SerializeField] private GameObject menuPause;
+
+
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        botonPause.SetActive(false);
+        menuPause.SetActive(true);
+    }
+
+    public void Reanudar()
+    {
+        Time.timeScale = 1f;
+        botonPause.SetActive(true);
+        menuPause.SetActive(false);
+    }
+
+    public void Reiniciar()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Cerrar ()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+}
