0bd5d83978b19d051d841696010b0c633ddb0c08
ROGER CRUZ <rogeercruz10@gmail.com>
2023-05-03 17:03:09 +0200

    ULTIMA ESCENA

---
+++ Avatar Multi Fight/Assets/Scenes/carpeta_noia/PEGA_BOSS.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PEGA_BOSS : MonoBehaviour
+{
+    public int damage;
+    public GameObject Chica2;
+
+    public float time_to_hit;
+    public float timeCooldown;
+
+    public float time_Visual;
+    Animator Anim1;
+
+    void Start()
+    {
+        Chica2 = GameObject.FindGameObjectWithTag("Chica2");
+        Anim1 = gameObject.GetComponent<Animator>();
+        timeCooldown = 1f;
+    }
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+
+        if (col.tag == "Chica2")
+        {
+            if (Time.timeSinceLevelLoad > time_to_hit)
+            {
+                time_to_hit = Time.timeSinceLevelLoad + timeCooldown;
+                DatosJugador.vidaPlayer -= damage;
+                Anim1.SetTrigger("PUNO");
+            }
+
+            //Chica2.GetComponent<DatosJugador>().restarVida(damage);
+
+
+        }
+
+        if (col.tag == "Enemigo")
+        {
+
+        }
+    }
+}
