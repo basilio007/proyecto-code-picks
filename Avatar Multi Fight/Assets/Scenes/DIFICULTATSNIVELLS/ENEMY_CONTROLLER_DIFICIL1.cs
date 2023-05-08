f5a5c3027d2bdb5fcf62f5d26092d756100ad6f7
ROGER CRUZ <rogeercruz10@gmail.com>
2023-05-08 09:25:32 +0200

    ESCENA DIFICIL - PRIMERA FETA

--- Avatar Multi Fight/Assets/Scenes/DIFICULTATSNIVELLS/ENEMY_CONTROLLER_DIFICIL1.cs
+++ Avatar Multi Fight/Assets/Scenes/DIFICULTATSNIVELLS/ENEMY_CONTROLLER_DIFICIL1.cs
@@ -1,18 +1,178 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Linq;
+using UnityEngine.SceneManagement;
 public class ENEMY_CONTROLLER_DIFICIL1 : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+
+    public int Id_Enemy;
+
+
+    /* [SerializeField] private float velocidaddemovimiento;
+      [SerializeField] private Transform[] puntordemovimiento; // puntos seria aixo
+      [SerializeField] private float distancia;
+      private int numrandom;
+      private SpriteRenderer spriteRenderer;
+
+
+
+      [SerializeField] private GameObject[] enemigos;
+      [SerializeField] private float tiempoEnemigos;
+      private float tiempoEnemigosSiguiente;
+      //bool canJump;
+      Animator anim1;
+
+
+      //public int damage; //da�o barra de vida
+
+
+      void Start()
+      {
+          anim1 = gameObject.GetComponent<Animator>();
+          numrandom = Random.Range(0, puntordemovimiento.Length);
+          //spriteRenderer = GetComponent<SpriteRenderer>();
+          //Girar();
+      }
+
+     void Update()
+     {
+         transform.position = Vector2.MoveTowards(transform.position, puntordemovimiento[numrandom].position, velocidaddemovimiento * Time.deltaTime);
+         if (Vector2.Distance(transform.position, puntordemovimiento[numrandom].position) < distancia)
+         {
+
+             numrandom = Random.Range(0, puntordemovimiento.Length);
+            // Girar();
+
+             //animacion pu�o
+
+
+
+
+             tiempoEnemigosSiguiente += Time.deltaTime;
+             if (tiempoEnemigosSiguiente >= tiempoEnemigos)
+             {
+                 tiempoEnemigosSiguiente = 0;
+                 Crearenemigos();
+             }
+         }
+     }
+
+
+         void Crearenemigos()
+          {
+              numrandom = Random.Range(0, puntordemovimiento.Length);
+              transform.position = Vector2.MoveTowards(transform.position, puntordemovimiento[numrandom].position, velocidaddemovimiento * Time.deltaTime);
+
+              Instantiate(enemigos[numrandom], transform.position, Quaternion.identity);
+          }
+
+         /* void Girar()
+          {
+              if (transform.position.x < puntordemovimiento[numrandom].position.x)
+              {
+                  spriteRenderer.flipX = true;
+
+              }
+              else
+              {
+                  spriteRenderer.flipX = false;
+              }
+          }
+
+      private float tiempo;
+
+       void OnCollisionEnter2D(Collision2D other)
+      {
+
+      }
+
+      //CUANDO DETECTA A LA CHICA LE PEGA UN PU�ETAZO
+       void OnCollisionStay2D(Collision2D collision)
+      {
+          if (collision.collider.CompareTag("Chica2"))
+          {
+
+
+          }
+      }
+  }
+
+    ////////////////////////////////////////modificacio meva//////////////////////
+
+ */
+
+
+
+
+    /// <summary>
+    /// ///////////////////////////////////
+    /// </summary>
+
+
+
+
+
+
+
+    /////////////////////////////////////////////////buena////////////////////////////////////////////////////////
+
+
+    [SerializeField] private float velocidaddemovimiento;
+    [SerializeField] private Transform[] puntordemovimiento; // puntos seria aixo
+    [SerializeField] private float distancia;
+    private int numrandom;
+    private SpriteRenderer spriteRenderer;
+
+
+
+    [SerializeField] private GameObject[] enemigos;
+    [SerializeField] private float tiempoEnemigos;
+    private float tiempoEnemigosSiguiente;
+    //bool canJump;
+    Animator anim1;
+
+
+    //public int damage; //da�o barra de vida
+
+
     void Start()
     {
-        
+        Id_Enemy = 0;
+        anim1 = gameObject.GetComponent<Animator>();
+        numrandom = Random.Range(0, puntordemovimiento.Length);
+        //spriteRenderer = GetComponent<SpriteRenderer>();
+        //Girar();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        tiempoEnemigosSiguiente += Time.deltaTime;
+        if (tiempoEnemigosSiguiente >= tiempoEnemigos)
+        {
+            tiempoEnemigosSiguiente = 0;
+            Crearenemigos();
+        }
     }
+
+    private void Crearenemigos()
+    {
+        Id_Enemy++;
+        //creara 2 enemics més 
+        if (Id_Enemy <= 2)
+        {
+
+            int numeroEnemigo = Random.Range(0, enemigos.Length);
+            Vector2 posicioAleatoria = new Vector2(Random.Range(-10, 10), Random.Range(0, 0));
+            var newObject = Instantiate(enemigos[numeroEnemigo], posicioAleatoria, Quaternion.identity);
+            newObject.name = "JUGADOR_1_" + Id_Enemy.ToString();
+
+        }
+        else
+        {
+            SceneManager.LoadScene("ice scene.dificil", LoadSceneMode.Single);
+        }
+    }
+
 }
